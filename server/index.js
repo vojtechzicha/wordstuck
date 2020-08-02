07597c602ded875629a675c1e8aaef49ba5eb28f
Vojtěch Zicha <vojtech@zicha.name>
2020-08-02 11:22:44 +0200

    Show port number in start-up logs

--- server/index.js
+++ server/index.js
@@ -54,20 +54,17 @@ console.log('2) Morgan Configuration - DONE')
 
 console.log('3. MongoDB Configuration')
 
-MongoClient.connect(
-  process.env.MONGO_URI,
-  (err, conn) => {
-    if (err) {
-      console.error('No connection to the database ' + process.env.MONGO_URI)
-      throw err
-    }
-    app.locals.db = conn.db(process.env.MONGO_DATABASE)
-
-    console.log('3) MongoDB Configuration - DONE')
-    console.log('4. Listening')
-
-    app.listen(process.env.PORT || 3030)
+MongoClient.connect(process.env.MONGO_URI, (err, conn) => {
+  if (err) {
+    console.error('No connection to the database ' + process.env.MONGO_URI)
+    throw err
   }
-)
+  app.locals.db = conn.db(process.env.MONGO_DATABASE)
+
+  console.log('3) MongoDB Configuration - DONE')
+  console.log(`4. Listening on port ${process.env.PORT || 3030}`)
+
+  app.listen(process.env.PORT || 3030)
+})
 
 app.use('/api/v1/items', items)
